5c2c39f64182312a1e129f67864f76af70214894
Peter Wibeck <pwibeck@gmail.com>
2015-07-24 13:28:11 +0200

    Comment out not used test

--- InstallerVerificationLibraryTest/ProcmonDataExtractorTests.cs
+++ InstallerVerificationLibraryTest/ProcmonDataExtractorTests.cs
@@ -9,18 +9,18 @@ namespace InstallerTestingLibraryTest
     [TestClass]
     public class ProcmonDataExtractorTests
     {
-        [TestMethod]
-        [DeploymentItem(@"TestTraceData.xml")]
-        public void GetEventlist_RepairScenario()
-        {
-            var eventList = ProcmonDataExtractor.GetEventlist("TestTraceData.xml");
+        //[TestMethod]
+        //[DeploymentItem(@"TestTraceData.xml")]
+        //public void GetEventlist_RepairScenario()
+        //{
+        //    var eventList = ProcmonDataExtractor.GetEventlist("TestTraceData.xml");
 
-            ProcmonDataExtractor extractor = new ProcmonDataExtractor();
-            extractor.ExtractData(ProcmonDataExtractor.TypeOfInstaller.Msi, null, eventList);
+        //    ProcmonDataExtractor extractor = new ProcmonDataExtractor();
+        //    extractor.ExtractData(ProcmonDataExtractor.TypeOfInstaller.Msi, null, eventList);
 
-            Assert.AreEqual(5, extractor.FileChanges.ToList().Count);
-            Assert.IsFalse(extractor.FileChanges.Any(x=> x.Type == FileChangeType.Delete));
-            Assert.IsFalse(extractor.FileChanges.Any(x => x.Type == FileChangeType.Rename));
-        }
+        //    Assert.AreEqual(5, extractor.FileChanges.ToList().Count);
+        //    Assert.IsFalse(extractor.FileChanges.Any(x=> x.Type == FileChangeType.Delete));
+        //    Assert.IsFalse(extractor.FileChanges.Any(x => x.Type == FileChangeType.Rename));
+        //}
     }
 }
